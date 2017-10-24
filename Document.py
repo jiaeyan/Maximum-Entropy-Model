ac99b5e5d83321ae8f7e2c66104135e4fed17775
Jiajie Yan <svenyan234@gmail.com>
2017-10-24 03:03:00 -0400

    Add files via upload

---
+++ Document.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 15, 2017
+@author: jiajieyan
+'''
+# A class representing the instance characteristics for the MaxEnt model.
+
+class Document():
+    
+    def __init__(self, data, label):
+        self.label = label
+        self.features = self.process_feature(data)
+        self.feature_vector = []
+        
+    def process_feature(self, data):
+        '''A bag-of-word approach to process raw data, more fancy methods can be applied.
+           Default setting is Multinomial, set() the result if a Bernoulli is preferred. 
+        '''
+        return data.split() 
